7859d8ac2ac8f9c01cfae6b5097533fdda519146
Hixo <hixohixo32@gmail.com>
2023-02-05 19:52:25 +0100

    add header

---
+++ Client/src/components/Header.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+    <header class="w-full h-16 flex items-center text-white">
+        <div class="m-6"><h1 class="text-3xl">BitLy</h1></div>
+    </header>
+</template>
