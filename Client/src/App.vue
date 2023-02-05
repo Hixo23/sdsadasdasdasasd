f9c5a1c0154786999417ec29205cca681486e876
Hixo <hixohixo32@gmail.com>
2023-02-05 20:27:37 +0100

    add hero section

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Header from './components/Header.vue';
+import Hero from './components/Hero.vue'
 </script>
 
 <template>
- <div class="w-screen h-screen bg-gray-800 font-[Poppins]">
+ <div class="w-screen h-screen bg-gray-800 font-jetbrainsMono">
   <Header />
+  <Hero />
  </div>
 </template>
 
---
+++ Client/src/components/Hero.vue
@@ -0,0 +1,13 @@
+<script></script>
+
+<template>
+    <div class="w-full h-auto overflow-hiddenflex flex-col items-center space-y-12">
+        <h2 class="text-white text-3xl text-center">Create Short Links!</h2>
+        <form action="">
+            <div class="bg-gray-600 w-[28rem] py-12 gap-4 mx-auto flex justify-center rounded-lg">
+                    <input class="py-2 px-4 rounded-xl" placeholder="Paste a link" type="text">
+                    <button class="bg-blue-500 py-2 px-4 rounded-xl">Shorten</button>
+            </div>
+        </form>
+    </div>
+</template>
