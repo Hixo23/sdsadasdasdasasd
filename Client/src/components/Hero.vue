1a1f6bbb37916df54fb74bfae910407018266e0b
Hixo <hixohixo32@gmail.com>
2023-02-05 23:39:40 +0100

    add async to createLink

--- Client/src/components/Hero.vue
+++ Client/src/components/Hero.vue
@@ -1,44 +1,50 @@
 <script>
 
-    export default {
-        data() {
-            return {
-                json: {
-                    name: "",
-                    url: ""
-                },
-                errors: {
-                    name: "",
-                    url: ""
-                },
-                valid: false
-            }
-        },
-        methods: {
-            createLink() {
+export default {
+    data() {
+        return {
+            json: {
+                name: "",
+                url: ""
+            },
+            errors: {
+                name: "",
+                url: ""
+            },
+            valid: false
+        }
+    },
+    methods: {
+        async createLink() {
+            this.valid = true
+            if (this.json.name.trim().length < 4) {
+                this.valid = false;
+                this.errors.name = "Name must be at least 4 character long"
+            } else {
                 this.valid = true
-                if(this.json.name.trim().length < 4) {
-                    this.valid = false;
-                    this.errors.name = "Name must be at least 4 character long"
-                } else {
-                    this.valid = true
-                    this.errors.name = ""
-                }
+                this.errors.name = ""
+            }
 
-                if(this.json.url.trim().length < 4) {
-                    this.valid = false;
-                    this.errors.url = "URL must be at least 4 character long"
-                } else {
-                    this.valid = true
-                    this.errors.url = ""
-                }
+            if (this.json.url.trim().length < 4) {
+                this.valid = false;
+                this.errors.url = "URL must be at least 4 character long"
+            } else {
+                this.valid = true
+                this.errors.url = ""
+            }
 
-                if(this.valid) {
-                    console.log(this.json)
-                }
+            if (this.valid) {
+                fetch('http://127.0.0.1:3000/links', {
+                    method: "POST",
+                    body: JSON.stringify(this.json),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
             }
         }
     }
+}
 </script>
 
 <template>
@@ -48,13 +54,14 @@
             <div class="bg-gray-600 w-[28rem] py-12 gap-4 mx-auto flex flex-col justify-center rounded-lg">
                 <div class="mx-auto">
                     <input class="py-2 px-4 rounded-xl w-96" v-model="json.url" placeholder="Paste a link" type="text">
-                    <p class="text-red-600">{{ errors.url }}</p>
+                    <p class="text-red-600 mt-2">{{ errors.url }}</p>
                 </div>
                 <div class="mx-auto">
                     <input class="py-2 px-4 rounded-xl w-96" v-model="json.name" placeholder="Type a name" type="text">
-                    <p class="text-red-600">{{ errors.name }}</p>
+                    <p class="text-red-600 mt-2">{{ errors.name }}</p>
                 </div>
-                <button type="button" @click="createLink()" class="bg-blue-500 px-8 py-2 rounded-xl w-96 mx-auto hover:bg-transparent hover:border hover:border-blue-500 hover:text-white transition-all duration-150">Shorten</button>
+                <button type="button" @click="createLink()"
+                    class="bg-blue-500 px-8 py-2 rounded-xl w-96 mx-auto hover:bg-transparent hover:border hover:border-blue-500 hover:text-white transition-all duration-150">Shorten</button>
             </div>
         </form>
     </div>
