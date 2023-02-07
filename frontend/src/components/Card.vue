c4f0c0ddcd0fa65a0d5d570c34ab11313cfa8297
Hixo <hixohixo32@gmail.com>
2023-02-07 15:40:53 +0100

    card

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,7 +1,14 @@
 <script setup>
+import axios from 'axios';
 const props = defineProps(['link'])
 
     const { ID, url, name } = props.link
+
+    const deleteLink = () => {
+        axios.delete(`http://localhost:3000/links/${ID}`).then(res => {
+            console.log(res)
+        })
+    }
 </script>
 <template>
     <div class="bg-[#0F0B0B] w-[28rem] h-32 text-white flex flex-col justify-center items-center rounded-lg">
@@ -10,7 +17,7 @@ const props = defineProps(['link'])
         <a>Link: http://localhost:3000/links/r/{{ name }}</a>
         <div class="text-white text-2xl flex justify-center gap-4">
             <button><i class="fa-solid fa-pen-to-square"></i></button>
-            <button><i class="fa-solid fa-trash"></i></button>
+            <button @click="deleteLink()"><i class="fa-solid fa-trash"></i></button>
         </div>
     </div>
 </template>
